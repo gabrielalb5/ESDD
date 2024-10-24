f36955420dd7109ac154d18772ea96a5bd649483
gabrielalb5 <gabrielalbino28@gmail.com>
2024-10-24 19:23:05 -0300

    teste quick e merge 100k e 1M

--- ex_avaliativo1/main.c
+++ ex_avaliativo1/main.c
@@ -5,13 +5,12 @@ int main(){
     printf("-----TESTE NUMERO %d-----\n\n",i+1);
 
     printf("MELHOR CASO - VETORES ORDENADOS\n\n");
-    melhor(1000);
+    melhor(100000);
+    melhor(1000000);
 
     printf("PIOR CASO - VETORES INVERTIDOS\n\n");
-    pior(1000);
-
-    printf("CASO MÉDIO - VETORES ALEATÓRIOS\n\n");
-    medio(1000);
+    pior(100000);
+    pior(1000000);
     }
 
     return 0;
