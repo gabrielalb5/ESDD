6fbe36a4b838a2664bfa2553dccc3e6383345b57
gabrielalb5 <gabrielalbino28@gmail.com>
2024-10-25 12:32:23 -0300

    fim

--- ex_avaliativo1/main.c
+++ ex_avaliativo1/main.c
@@ -2,7 +2,6 @@
 
 int main(){
 
-    for(int i=0;i<3;i++){
     printf("-----TESTE NUMERO %d-----\n\n",i+1);
 
     printf("MELHOR CASO - VETORES ORDENADOS\n\n");
@@ -10,7 +9,9 @@ int main(){
 
     printf("PIOR CASO - VETORES INVERTIDOS\n\n");
     pior(1000);
-    }
+
+    printf("CASO MÉDIO - VETORES ALEATÓRIOS\n\n");
+    medio(1000);
 
     return 0;
 }
