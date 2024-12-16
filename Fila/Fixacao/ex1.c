b1da8475b11b5480702bc24b60304820525c874e
gabrielalb5 <gabrielalbino28@gmail.com>
2024-12-15 22:14:23 -0300

    ex 1 e 2

--- Fila/Fixacao/ex1.c
+++ Fila/Fixacao/ex1.c
@@ -84,49 +84,45 @@ bool dequeue(T_Queue queue, T_Info* elemento) {
     return false;
 }
 
+
 int main() {
-    int a, b;
-    T_Queue queueA = init(5);
-    enqueue(queueA, 2);
-    enqueue(queueA, 3);
-    enqueue(queueA, 5);
-    enqueue(queueA, 7);
-    enqueue(queueA, 12);
+    T_Queue fila;
+    T_Queue auxiliar;
+    T_Info input;
+    fila = init(20);
 
-    T_Queue queueB = init(8);
-    enqueue(queueB, 3);
-    enqueue(queueB, 6);
-    enqueue(queueB, 5);
-    enqueue(queueB, 7);
-    enqueue(queueB, 10);
-    enqueue(queueB, 11);
-    enqueue(queueB, 13);
-    enqueue(queueB, 0);
-
-    T_Queue auxiliarA = init(5);
-    T_Queue auxiliarB = init(8);
-
-    printf("Relatório 1\n");
-    while (!is_empty(queueA)) {
-        dequeue(queueA, &a);
-        enqueue(auxiliarA, a);
-        while (!is_empty(queueB)) {
-            dequeue(queueB, &b);
-            enqueue(auxiliarB, b);
-
-            if (a == b) {
-                printf("%d\n", a);
-            }
-        }
-        while (!is_empty(auxiliarB)) {
-            dequeue(auxiliarB, &b);
-            enqueue(queueB, b);
+    for(int i = 0; i < 20; i++) {
+        printf("Digite valor: ");
+        scanf("%d", &input);
+        if (enqueue(fila, input) ) {
+            printf("%d inserido com sucesso.\n", input);
+        } else {
+            printf("Erro ao inserir %d.\n", input);
         }
     }
-    while (!is_empty(auxiliarA)) {
-        dequeue(auxiliarA, &a);
-        enqueue(queueA, a);
+
+    auxiliar = init(20);
+    while (!is_empty(fila)) {
+        dequeue(fila, &input);
+
+        if (input == 0) {
+            enqueue(auxiliar, input);
+        } else if (input % 2 == 1) {
+            enqueue(auxiliar, input);
+        }
     }
+
+    while (!is_empty(auxiliar)) {
+        dequeue(auxiliar, &input);
+        enqueue(fila, input);
+    }
+
+    printf("Inicio >\n");
+    while(!is_empty(fila)) {
+        dequeue(fila, &input);
+        printf("%d\n", input);
+    }
+    printf("< Fim\n");
 
     return 0;
 }
